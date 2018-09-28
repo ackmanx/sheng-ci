17d910e4c889167ebbcf0b5553fee696ceb06aab
Eric Majerus <>
2018-09-27 22:29:28 -0500

    Added API calls to webtask.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import './app.css'
 
+const webtaskAPI = 'https://wt-b799f0ade639c484ac317ecb184a02ad-0.sandbox.auth0-extend.com/sheng-ci'
+
 export default class App extends React.Component {
     constructor() {
         super()
 
         this.state = {
-            people: []
+            vocab: {}
         }
 
-        const webtaskBaseUrl = 'https://wt-b799f0ade639c484ac317ecb184a02ad-0.sandbox.auth0-extend.com'
+        this.fuckingGetIt = this.fuckingGetIt.bind(this)
+        this.fuckingSendIt = this.fuckingSendIt.bind(this)
 
-        fetch(`${webtaskBaseUrl}/people`)
+        this.fuckingGetIt()
+    }
+
+    render() {
+        const {vocab} = this.state
+
+        return (
+            <div>
+                <h1>Motha-fukkin-vocabulary</h1>
+
+                {Object.keys(vocab).map(id => {
+                    const entry = vocab[id]
+                    return (
+                        <div key={Math.floor(Math.random() * 1000)}>
+                            <div>{entry.hanzi}</div>
+                            <div>{entry.pinyin}</div>
+                            <div>{entry.english}</div>
+                            <hr/>
+                        </div>
+                    )
+                })}
+
+                <input id='hanzi' onChange={e => this.setState({hanzi: e.target.value})}/>
+                <input id='pinyin' onChange={e => this.setState({pinyin: e.target.value})}/>
+                <input id='english' onChange={e => this.setState({english: e.target.value})}/>
+
+                <button onClick={this.fuckingSendIt}>Fucking send it!</button>
+            </div>
+        )
+    }
+
+    fuckingGetIt() {
+        fetch(webtaskAPI)
             .then(res => {
                 if (res.status !== 200) {
                     console.error('Uh oh. The webtask did not work!')
@@ -19,20 +54,17 @@ export default class App extends React.Component {
                 }
 
                 res.json().then(json => {
-                    this.setState({people: json})
+                    this.setState({vocab: json})
                 })
             })
             .catch(e => console.error(e))
     }
 
-    render() {
-        const {people} = this.state
+    fuckingSendIt() {
+        const queryParams = `id=${new Date().getTime()}&hanzi=${encodeURI(this.state.hanzi)}&pinyin=${this.state.pinyin}&english=${this.state.english}`
 
-        return (
-            <div>
-                <h1>Oh yeah baby!</h1>
-                <div>{JSON.stringify(people)}</div>
-            </div>
-        )
+        fetch(`${webtaskAPI}?${queryParams}`, {method: 'POST'})
+            .then(res => this.fuckingGetIt())
+            .catch(err => console.error(err))
     }
 }
---
+++ src/webtask-endpoint.js
@@ -0,0 +1,62 @@
+/**
+ * This file needs to be copy and pasted into webtask.io/make UI
+ * Or, I could download their CLI like a smart person and use that
+ *
+ * @param context {WebtaskContext}
+ */
+module.exports = function (context, req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+
+    switch (req.method) {
+        case 'GET':
+            get(context, res)
+            break;
+
+        case 'POST':
+            post(context, req, res)
+            break;
+    }
+}
+
+function get(context, res) {
+    context.storage.get(function (error, data = {}) {
+        let body
+
+        if (error) {
+            body = {
+                error,
+                message:  'Error getting from storage'
+            }
+        }
+        else {
+            body = data
+        }
+
+        res.end(JSON.stringify(body))
+    })
+}
+
+function post(context, req, res) {
+    context.storage.get(function (error, data = {}) {
+        if (error) {
+            return res.end(JSON.stringify({error: 'Error getting from storage before saving', message: error}))
+        }
+
+        if (req.query.id) {
+            data[req.query.id] = {
+                category: req.query.category,
+                hanzi: decodeURI(req.query.hanzi),
+                pinyin: req.query.pinyin,
+                english: req.query.english,
+            }
+        }
+
+        context.storage.set(data, function (error) {
+            if (error) {
+                return res.end(JSON.stringify({error: 'Error saving to storage', message: error}))
+            }
+
+            res.end(JSON.stringify({message: 'Saved!', saved: data}))
+        })
+    })
+}
