92a25a0a10e8cc5da7941c99de92a6b35a3d7006
Eric Majerus <fake@fake.com>
2018-09-29 00:07:20 -0500

    sidebar dummy html/css

--- src/app.js
+++ src/app.js
@@ -10,8 +10,62 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div>
-                
+            <div className='app'>
+                <div className='sidebar'>
+                    <div className='add-category'>+ New Category</div>
+                    <ul>
+                        <li>Names and Places</li>
+                        <li>Grammar</li>
+                        <li>The Rest</li>
+                    </ul>
+                </div>
+                <div className='entries-panel'>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                </div>
             </div>
         )
     }
