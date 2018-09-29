84ca61e34dbcae04cae28c55afa10d8e845e29f4
Eric Majerus <fake@fake.com>
2018-09-28 23:20:35 -0500

    cleaned up webtask entries. added webtask categories.

--- webtask/sheng-ci-category.js
+++ webtask/sheng-ci-category.js
@@ -2,7 +2,7 @@
  * This file is hosted on Webtask
  * To update, run npm script webtask-update
  *
- * https://wt-b799f0ade639c484ac317ecb184a02ad-0.sandbox.auth0-extend.com/sheng-ci-entry
+ * https://wt-b799f0ade639c484ac317ecb184a02ad-0.sandbox.auth0-extend.com/sheng-ci-category
  */
 const express = require('express')
 const Webtask = require('webtask-tools')
@@ -12,36 +12,32 @@ const app = express()
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-    req.webtaskContext.storage.get(function (error, entries = {}) {
+    req.webtaskContext.storage.get(function (error, categories = {}) {
         if (error) {
             return res.json({message: 'Error getting from storage', error})
         }
 
-        res.json(entries)
+        res.json(categories)
     })
 })
 
 app.post('/', function (req, res) {
-    req.webtaskContext.storage.get(function (error, entries = {}) {
+    req.webtaskContext.storage.get(function (error, categories = {}) {
         if (error) {
             return res.json({message: 'Error getting from storage before saving', error})
         }
 
-        if (req.query.id) {
-            entries[req.query.id] = {
-                category: req.query.category,
-                hanzi: decodeURI(req.query.hanzi),
-                pinyin: req.query.pinyin,
-                english: req.query.english,
-            }
+        categories[req.body.id] = {
+            id: req.body.id,
+            name: req.body.name,
         }
 
-        req.webtaskContext.storage.set(entries, function (error) {
+        req.webtaskContext.storage.set(categories, function (error) {
             if (error) {
                 return res.json({message: 'Error saving to storage', error})
             }
 
-            res.json({message: 'Saved!', saved: entries})
+            res.json({message: 'Saved!', saved: categories[req.body.id]})
         })
     })
 })
