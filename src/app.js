dfc14a348ae0ef4754319cf69eec6ff89c95ce6d
Eric Majerus <fake@fake.com>
2018-09-29 00:16:35 -0500

    entries dummy html/css

--- src/app.js
+++ src/app.js
@@ -21,9 +21,49 @@ export default class App extends React.Component {
                 </div>
                 <div className='entries-panel'>
                     <div className='entry'>
-                        <div>你好</div>
-                        <div>ni3 hao3</div>
-                        <div>hello</div>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
                     </div>
                     <div className='entry'>
                         <div>你好</div>
@@ -64,6 +104,56 @@ export default class App extends React.Component {
                         <div>你好</div>
                         <div>ni3 hao3</div>
                         <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>你好</div>
+                        <div>ni3 hao3</div>
+                        <div>hello</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
+                    </div>
+                    <div className='entry'>
+                        <div>为什么你不爱我</div>
+                        <div>wei4 shen2 me ni3 nu4 ai4 wo3</div>
+                        <div>why don't you love me</div>
                     </div>
                 </div>
             </div>
