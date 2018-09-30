4e6e110355f0949bff708a527b6edf102a5afe91
Eric Majerus <fake@fake.com>
2018-09-30 11:45:38 -0500

    removed reducers index.js file because it adds no value. i can separate the whole store create to another file if i need separation.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './components/app'
 import './index.css'
 import {Provider} from 'react-redux'
-import {applyMiddleware, createStore} from 'redux'
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import combinedReducers from './reducers'
+import app from './reducers/app'
+import categories from './reducers/categories'
+import entries from './reducers/entries'
 
 const initialState = {
     app: {},
@@ -15,7 +17,11 @@ const initialState = {
 }
 
 const store = createStore(
-    combinedReducers,
+    combineReducers({
+        app,
+        categories,
+        entries,
+    }),
     initialState,
     composeWithDevTools(
         applyMiddleware(thunk),
